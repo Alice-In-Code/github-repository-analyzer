d6b0ba1950adf5cc959f8cc06f50723871505e27
Alice-In-Code <195574041+Alice-In-Code@users.noreply.github.com>
2026-06-19 03:27:46 +0100

    test: add additional GitHub service tests

--- src/tests/test_github_api.py
+++ src/tests/test_github_api.py
@@ -4,7 +4,11 @@ Automated tests for the GitHub API service.
 
 from unittest.mock import Mock, patch
 
-from src.services.github.repository_api import get_repository
+from src.services.github.repository_api import (
+    get_repository,
+    get_repository_items_count,
+    get_pull_requests_count
+)
 
 
 @patch("src.services.github.repository_api.requests.get")
@@ -44,3 +48,64 @@ def test_get_repository_not_found(mock_get: Mock) -> None:
     mock_get.assert_called_once_with("https://api.github.com/repos/owner/repository")
 
     assert repository is None
+
+
+@patch("src.services.github.repository_api.requests.get")
+def test_get_repository_items_count_success(mock_get: Mock) -> None:
+    """
+        Verify repository items count is successful.
+
+        Args:
+            mock_get:
+                Mock object used to verify the external call successfully returns items count.
+    """
+
+    response = Mock()
+
+    response.status_code = 200
+
+    response.json.return_value = {
+        "total_count": 0
+    }
+
+    mock_get.return_value = response
+
+    result = get_repository_items_count(
+        "owner/repository",
+        "closed"
+    )
+
+    mock_get.assert_called_once()
+
+    assert result == 0
+
+
+@patch("src.services.github.repository_api.requests.get")
+def test_get_pull_requests_count_success(mock_get: Mock) -> None:
+    """
+        Verify repository items count is successful.
+
+        Args:
+            mock_get:
+                Mock object used to verify the external call successfully returns pull request count.
+    """
+
+    response = Mock()
+
+    response.status_code = 200
+
+    response.headers = {}
+
+    response.json.return_value = [
+        {},
+        {}
+    ]
+
+    mock_get.return_value = response
+
+    result = get_pull_requests_count(
+        "owner/repository",
+        "open"
+    )
+
+    assert result == 2
